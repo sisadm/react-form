57ad7d27f1c4307d8d2b6242dfa721f2795dd159
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-11-19 23:36:51 +0000

    adding form into the main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Form from './components/Form'
 
 function App() {
 
 
   return (
-      <h1>Hello</h1>
+      <div>
+        <h1>Hello</h1>
+        <Form />
+      </div>
       )
 }
 
